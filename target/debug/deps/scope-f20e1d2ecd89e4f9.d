a460211dd72e7f0cdc7ac97762caedb3cc0519b4
os芜湖队 <2331249337@qq.com>
2021-08-27 16:38:25 +0800

    material

---
+++ target/debug/deps/scope-f20e1d2ecd89e4f9.d
@@ -0,0 +1,5 @@
+c:\Users\wanghuasheng\Desktop\studyRust\rust_demo\target\debug\deps\scope-f20e1d2ecd89e4f9.rmeta: src\main.rs
+
+c:\Users\wanghuasheng\Desktop\studyRust\rust_demo\target\debug\deps\scope-f20e1d2ecd89e4f9.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/scope.d
@@ -0,0 +1,1 @@
+c:\Users\wanghuasheng\Desktop\studyRust\rust_demo\target\debug\scope.exe: c:\Users\wanghuasheng\Desktop\studyRust\rust_demo\src\main.rs
