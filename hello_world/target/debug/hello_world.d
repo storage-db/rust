5c9b3714901d4ba28b671b78e15cc12696bfe0e6
cargo-touth <2331249337@qq.com>
2023-02-25 15:26:36 +0800

    feat : add demo for satring

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/ubuntu/rust/hello_world/target/debug/hello_world: /home/ubuntu/rust/hello_world/src/main.rs
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/home/ubuntu/rust/hello_world/target/release/hello_world: /home/ubuntu/rust/hello_world/src/main.rs
